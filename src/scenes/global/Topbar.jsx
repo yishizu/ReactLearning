a6ebf748748b3aeca5e7848b555fc4c3e7ddd552
yishizu <48244588+yishizu@users.noreply.github.com>
2023-04-06 14:26:31 +0900

    Add topbar

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,56 @@
+import {Box, IconButton, useColorScheme, useTheme} from "@mui/material";
+import {useContex} from "react";
+import { ColorModeContext } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+
+
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NortificationOutlinedIcon from "@mui/icons-material/NortificationedOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const Topbar  =() =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
+    return <Box display='flex' justifyContent="space-between" p={2}>
+        /**
+         * Search
+         */
+         <Box display="flex" backgroundColor = {colors.primary[400]} borderRadius="3px">
+            <InputBase sx={{ml:2, flex:1}} placeholder ="search here..."></InputBase>
+            <IconButton type="button" sx={{p:1}}>
+                <SearchIcon>
+
+                </SearchIcon>
+            </IconButton>
+         </Box>
+        <Box display="flex">
+            <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode =='dark' ?(
+                    <DarkModeOutlinedIcon />
+                ):(
+                    <LightModeOutlinedIcon/>
+                )}
+                <LightModeOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <NortificationOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                
+            </IconButton>
+        </Box>
+    </Box>
+}
+
+export default Topbar ;
